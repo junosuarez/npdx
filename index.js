7f671745a51700551f245d2471bd4b523b0dc84e
jden <jason@denizac.org>
2013-05-14 22:02:53 -0700

    add npdx sched

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var opener = require('opener')
 var npdx = require('commander')
+var _ = require('lodash')
 
 npdx.version(require('./package.json').version)
 
@@ -8,6 +9,32 @@ npdx.command('www')
   .action(function () {
     opener('http://nodepdx.org')
   })
+
+npdx.command('sched')
+  .description('print the schedule')
+  .action(function () {
+    require('lanyrd').schedule('nodepdx', '2013', function(err, res, body) { 
+
+    _(body).groupBy(function (s) { return s.day })
+      .forEach(function (day, title) {
+        console.log('== ' + title)
+        day.forEach(function (session) {
+          var pre = now(session) ? '> ' : '  '
+          var speaker = (session.speakers[0] || {}).title
+          console.log(pre + session.title + '   ' + (speaker || session.subtitle))
+        })
+      })
+
+    })
+  })
+
+// (session) => Boolean
+function now(session) {
+  var start = new Date(session.start_time + '-0700')
+  var end = new Date(session.end_time + '-0700')
+  var now = Date.now()
+  return now >= start && now <= end
+}
 
 
 npdx.parse(process.argv)
